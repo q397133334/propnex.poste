ba80bd6f452b3a2ebab2947e694a6bf317f28a16
fg <523291540@qq.com>
2022-09-16 17:07:56 +0800

    同上

--- Propnex.Poster.WinFrmGuru/Main.Designer.cs
+++ Propnex.Poster.WinFrmGuru/Main.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
@@ -56,5 +58,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
