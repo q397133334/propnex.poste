155885d76b913db3a76f842bdf45d14c1d1a08d4
fg <523291540@qq.com>
2022-09-16 17:05:57 +0800

    split sli

---
+++ Propnex.Poster.WinFrmGuru/Api/WebServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace Propnex.Poster.Guru.Api
+{
+    public class WebServer
+    {
+
+        public static string Id = System.Configuration.ConfigurationManager.AppSettings["Id"];
+
+        public static string BaseUrl = System.Configuration.ConfigurationManager.AppSettings["BaseUrl"];
+
+        public async Task<Dtos.PnTaskDto> GetTask()
+        {
+            string url = $"{BaseUrl}/api/app/pn-task/pn-task";
+
+            var result = await url.GetJsonAsync<Dtos.PnTaskDto>();
+
+            if (Guid.Empty == result.Id)
+            {
+                return null;
+            }
+            return result;
+        }
+    }
+}
