cda0fd651ce564d3d2d97ef888daff2589b421e2
fg <523291540@qq.com>
2023-08-06 10:58:23 +0800

    恢复动态获取任务

--- PropnexPoster.WPF/PosterRun.cs
+++ PropnexPoster.WPF/PosterRun.cs
@@ -1146,12 +1146,12 @@ namespace PropnexPoster.WPF
         private async Task<GuruTasks> getGuruTasks()
         {
             string context = "";
-            //taskDto = await WebServer.GetTask();
-            taskDto = new PnTaskDto()
-            {
-                Id = Guid.Parse("3a0c452d-4993-5729-9a89-c5cb725df655"),
-                Number = "941330.guru.tsk"
-            };
+            taskDto = await WebServer.GetTask();
+            //taskDto = new PnTaskDto()
+            //{
+            //    Id = Guid.Parse("3a0c452d-4993-5729-9a89-c5cb725df655"),
+            //    Number = "941330.guru.tsk"
+            //};
 
             if (taskDto != null)
             {
