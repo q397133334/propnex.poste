23efe0d0cc7dcb73f9c804bed3a76c3428217891
fangang <523291540@qq.com>
2024-06-04 11:57:09 +0800

    增加采集property 的定时服务

---
+++ Propnex.Poster.WebServer/BackgroundJobs/GetPropertyWorker.cs
@@ -0,0 +1,77 @@
+﻿using Volo.Abp.BackgroundWorkers;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Threading;
+using Propnex.Poster.WebServer.Entities;
+using Flurl.Http;
+using Propnex.Poster.PropertyGuru.Mobile;
+using Propnex.Poster.PropertyGuru.Mobile.Dto;
+using Propnex.Poster.PropertyGuru.Mobile.Model;
+
+namespace Propnex.Poster.WebServer.BackgroundJobs
+{
+    public class GuruMissingIdResult
+    {
+        public string status { get; set; }
+
+        public List<string> data { get; set; }
+    }
+
+    public class GetPropertyWorker : AsyncPeriodicBackgroundWorkerBase
+    {
+        public GetPropertyWorker(AbpAsyncTimer timer, IServiceScopeFactory serviceScopeFactory) : base(timer, serviceScopeFactory)
+        {
+            Timer.Period = 1000 * 60 * 60;//1 hour
+        }
+
+        [Volo.Abp.Uow.UnitOfWork(false)]
+        protected async override Task DoWorkAsync(PeriodicBackgroundWorkerContext workerContext)
+        {
+            var list = new List<string>();
+
+            var GuruMissingId = await "https://pa-production.propnex.net/index.php/scrape/getGuruMissingId".GetJsonAsync<GuruMissingIdResult>();
+
+            if (GuruMissingId.status != "ok")
+            {
+                return;
+            }
+            list = GuruMissingId.data;
+            var loginResult = await new Auth().LoginAsync(new AuthLogin()
+            {
+                UserName = "davidytp@gmail.com",
+                Password = "calista"
+            });
+            Token token;
+            if (loginResult.HttpStatusCode == System.Net.HttpStatusCode.OK)
+            {
+                token = loginResult.Data;
+            }
+            else
+            {
+                return;
+            }
+            var jsonList = new List<string>();
+
+            foreach (var item in list)
+            {
+                ProjectsApi api = new ProjectsApi(token);
+                var projectResult = await api.GetProjectAsync(int.Parse(item));
+                if (projectResult.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var project = projectResult.Data;
+                    jsonList.Add(project.ToJson());
+
+                    //https://pa-production.propnex.net/index.php/scrape/guruProjects
+                    //https://pa-staging.propnex.net/index.php/scrape/guruProjects
+                    var ok = await $"https://pa-production.propnex.net/index.php/scrape/guruProjects".PostUrlEncodedAsync(new
+                    {
+                        id = item,
+                        json = projectResult.Message
+                    });
+
+                    var msg = await ok.GetStringAsync();
+                    Console.WriteLine(msg);
+                }
+            }
+        }
+    }
+}
