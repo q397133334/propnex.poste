8e7a5332c3f4fa964b51dde1b41ef0989ff7c4d1
fangang <523291540@qq.com>
2025-09-15 17:43:02 +0800

    no message

--- Propnex.Poster.PropertyGuru.Mobile/ClientBase.cs
+++ Propnex.Poster.PropertyGuru.Mobile/ClientBase.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Net.NetworkInformation;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Polly;
 using Polly.Retry;
@@ -30,14 +31,15 @@ namespace Propnex.Poster.PropertyGuru.Mobile
             client = new RestSharp.RestClient(new RestClientOptions()
             {
                 BaseUrl = new Uri(baseUrl),
-                //UseDefaultCredentials = true,
+                UseDefaultCredentials = true,
                 UserAgent = ""
             });
-            //client.AddDefaultHeader(KnownHeaders.UserAgent, "sg;agentnet;android;2025.8.20;SM-N9760;7ae7893e-f212-49ab-ae59-c64808a8573d");
             Log?.Invoke($"client {baseUrl}");
             clientRetryPolicy = Policy
                 .Handle<Exception>()
                  .OrResult<RestResponse>(response =>
+                 response.StatusCode==HttpStatusCode.Forbidden||
+                 response.StatusCode == HttpStatusCode.Unauthorized ||
                  response.ResponseStatus == ResponseStatus.TimedOut ||
                  response.ResponseStatus == ResponseStatus.Aborted) //||
                                                                     //response.ResponseStatus == ResponseStatus.Error)
@@ -63,11 +65,12 @@ namespace Propnex.Poster.PropertyGuru.Mobile
 
             };
 
-            //client.AddDefaultHeader(KnownHeaders.UserAgent, "sg;agentnet;android;2025.8.20;SM-N9760;7ae7893e-f212-49ab-ae59-c64808a8573d");
             Log?.Invoke($"client {baseUrl}");
             clientRetryPolicy = Policy
                 .Handle<Exception>()
                  .OrResult<RestResponse>(response =>
+                 response.StatusCode == HttpStatusCode.Forbidden ||
+                 response.StatusCode == HttpStatusCode.Unauthorized ||
                  response.ResponseStatus == ResponseStatus.TimedOut ||
                  response.ResponseStatus == ResponseStatus.Aborted)//||
                                                                    //response.ResponseStatus == ResponseStatus.Error)
@@ -88,26 +91,32 @@ namespace Propnex.Poster.PropertyGuru.Mobile
 
         public RestRequest GetRequest()
         {
+            System.Threading.Thread.Sleep(Random.Next(1000, 5000));
             RestRequest request = new RestRequest();
             //request.AddHeader("x-clientid", "L7C9YKV9-ESF3606Q-GHF9H1F5-8LJMKRO5");
             //request.AddHeader("x-clientsecret", "jjiF916yVwfCRQEJtS6loHVDZ16mWPWf");
-            request.AddOrUpdateHeader(KnownHeaders.UserAgent, "sg;agentnet;android;2025.8.20;SM-N9760;727259e1-f2b7-4e9e-b71d-be34d5d52ebd");
+            request.AddOrUpdateHeader(KnownHeaders.UserAgent, "sg;agentnet;android;2025.8.20;SM-N9760");
             //request.AddHeader("sentry-trace", "b6dda4d0b7894eacadc6c899e8b062f5-b80b761182153526");
             //request.AddHeader("sentry-trace", "5776dbfd7f464c7e8a2a2befeabf67de-0dddf6e780b34bff");
             //request.AddHeader("baggage", "sentry-environment=SG-android,sentry-public_key=f0e2339e8daa4ee5a7051e17d4c4037b," +
             //    "sentry-release=com.allproperty.android.agentnet%402025.8.20%2B301413145," +
             //    "sentry-sample_rand=0.3413986372724464,sentry-trace_id=5776dbfd7f464c7e8a2a2befeabf67de");
             request.Timeout = new TimeSpan(0, 5, 0);// 1000 * 60 * 10;
-            //request.Version = new Version(2, 0);
+            request.Version = new Version(2, 0);
             return request;
         }
 
+
+        static Random Random = new Random();
+
         public RestRequest GetRequest(Method method, string resource)
         {
+            System.Threading.Thread.Sleep(Random.Next(1000, 5000));
+
             RestRequest request = new RestRequest();
             //request.AddHeader("x-clientid", "L7C9YKV9-ESF3606Q-GHF9H1F5-8LJMKRO5");
             //request.AddHeader("x-clientsecret", "jjiF916yVwfCRQEJtS6loHVDZ16mWPWf");
-            request.AddOrUpdateHeader(KnownHeaders.UserAgent, "sg;agentnet;android;2025.8.20;SM-N9760;7ae7893e-f212-49ab-ae59-c64808a8573d");
+            request.AddOrUpdateHeader(KnownHeaders.UserAgent, "sg;agentnet;android;2025.8.20;SM-N9760");
             request.Method = method;
             request.Timeout = new TimeSpan(0, 5, 0);// 1000 * 60 * 10;
             request.Resource = resource;
---
+++ Propnex.Poster.WebServer/Pages/TaskController.cs
@@ -0,0 +1,42 @@
+﻿using Flurl.Http;
+using Microsoft.AspNetCore.Mvc;
+using Propnex.Poster.PropertyGuru.Listing;
+using Propnex.Poster.PropertyGuru.Tasks;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Propnex.Poster.WebServer.Pages
+{
+    public class TaskController : AbpController
+    {
+        [Route("/api/task")]
+        [HttpGet]
+        public async Task<List<CreateOrUpdateListing>> GetListingJson(string listingId)
+        {
+            if (string.IsNullOrEmpty(listingId))
+                throw new UserFriendlyException("listingId can not be empty");
+            var listsContext = await $"https://pa-production.propnex.net/index.php/tasks/getListingFile?lid={listingId}".GetStringAsync();
+
+            try
+            {
+                var listings = new GuruTaskListings(listsContext);
+
+                var createOrUpdateListings = new List<CreateOrUpdateListing>();
+
+                foreach (var listing in listings.Listings)
+                {
+                    var createOrUpdateListing = new CreateOrUpdateListing();
+                    createOrUpdateListing.Create(listing.Listing);
+                    createOrUpdateListings.Add(createOrUpdateListing);
+                }
+
+                return createOrUpdateListings;
+            }
+            catch (Exception ex)
+            {
+                throw new UserFriendlyException(ex.Message, innerException: ex);
+            }
+
+        }
+    }
+}
